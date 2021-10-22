c4609f59acfb6a65ee98e343f86d538b05f54a18
JavierOG145 <57988663+JavierOG145@users.noreply.github.com>
2021-10-22 10:34:17 +0200

    Update Practica.py

--- Practica1/Practica.py
+++ Practica1/Practica.py
@@ -42,13 +42,18 @@ class Player(pygame.sprite.Sprite):
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect()
 
+        self.sonidoRising = pygame.mixer.Sound(ruta_rising)
+        self.sonidoFalling = pygame.mixer.Sound(ruta_falling)
+
 
     # Move the sprite based on user keypresses
     def update(self, pressed_keys):            
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
+            self.sonidoRising.play()
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0, 5)
+            self.sonidoFalling.play()
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-5, 0)
         if pressed_keys[K_RIGHT]:
@@ -107,6 +112,115 @@ class Cloud(pygame.sprite.Sprite):
         self.rect.move_ip(-self.speed, 0)
         if self.rect.right < 0:
             self.kill()
+
+# Setup for sounds. Defaults are good.
+pygame.mixer.init()
+
+# Load and play background music
+pygame.mixer.music.load(ruta_apoxode)
+pygame.mixer.music.play(loops=-1)
+
+# Initialize pygame
+pygame.init()
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Create a custom event for adding a new enemy
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
+# Create a custom event for adding a new cloud      
+ADDCLOUD = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDCLOUD, 1000)
+
+
+# Instantiate player. Right now, this is just a rectangle.
+player = Player()
+
+# Create groups to hold enemy sprites and all sprites
+# - enemies is used for collision detection and position updates
+# - all_sprites is used for rendering
+enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()     
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Variable to keep the main loop running
+running = True
+
+# Setup the clock for a decent framerate
+clock = pygame.time.Clock()
+
+# Main loop
+while running:
+    # for loop through the event queue
+    for event in pygame.event.get():
+        # Check for KEYDOWN event
+        if event.type == KEYDOWN:
+            # If the Esc key is pressed, then exit the main loop
+            if event.key == K_ESCAPE:
+                running = False
+        # Check for QUIT event. If QUIT, then set running to false.
+        elif event.type == QUIT:
+            running = False
+        # Add a new enemy?
+        elif event.type == ADDENEMY:
+            # Create the new enemy and add it to sprite groups
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(enemies)
+
+            #Create clouds
+            new_cloud = Cloud()
+            clouds.add(new_cloud) 
+            all_sprites.add(clouds)
+
+
+
+    # Get all the keys currently pressed
+    pressed_keys = pygame.key.get_pressed()
+
+    # Update the player sprite based on user keypresses
+    player.update(pressed_keys)
+
+    # Update enemy position
+    enemies.update()
+
+    # Update cloud position
+    clouds.update()
+
+
+    # Fill the screen with blue
+    screen.fill((135, 206, 250))
+
+    # Draw all sprites
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    #Check if any enemies have collided with the player
+    if pygame.sprite.spritecollideany(player, enemies):
+        #If so, then remove the player and stop the loop
+        sonidoCollision = pygame.mixer.Sound(ruta_collision)
+        sonidoCollision.play()
+
+
+        player.kill()
+        running = False
+
+    # Update the display
+    pygame.display.flip()
+
+    # Ensure program maintains a rate of 30 frames per second
+    clock.tick(30)
+
+
+# Done! Time to quit.
+pygame.mixer.music.stop()
+pygame.mixer.quit()
+pygame.quit()
+
 
 # Setup for sounds. Defaults are good.
 pygame.mixer.init()
